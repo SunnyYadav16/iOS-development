c11001f4150c28776c0195c861fcdc70dbd38953
Sunny Yadav <sunnyyadav@Sunnys-MacBook-Air.local>
2025-10-09 17:40:24 -0400

    Separating Code

---
+++ Separating Code Demo/Separating Code DemoTests/Separating_Code_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Separating_Code_DemoTests.swift
+//  Separating Code DemoTests
+//
+//  Created by Sunny Yadav on 9/25/25.
+//
+
+import Testing
+@testable import Separating_Code_Demo
+
+struct Separating_Code_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Separating Code Demo/Separating Code DemoUITests/Separating_Code_DemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Separating_Code_DemoUITestsLaunchTests.swift
+//  Separating Code DemoUITests
+//
+//  Created by Sunny Yadav on 9/25/25.
+//
+
+import XCTest
+
+final class Separating_Code_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
